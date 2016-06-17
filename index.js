a9ba35a7dec34ff2964f3235e2d45b90bd6d0fc1
drewfirment <drew.firment@gmail.com>
2016-06-17 12:31:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,51 +17,209 @@
  *  User: "Alexa, ask Beer Lover for a beer fact"
  *  Alexa: "Here's your beer fact: ..."
  */
+ /**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+'use strict';
+var aws = require('aws-sdk');
+var bucketname = "beer-facts";
+
+function AlexaSkill(appId) {
+    this._appId = appId;
+}
+
+AlexaSkill.speechOutputType = {
+    PLAIN_TEXT: 'PlainText',
+    SSML: 'SSML'
+}
+
+AlexaSkill.prototype.requestHandlers = {
+    LaunchRequest: function (event, context, response) {
+        this.eventHandlers.onLaunch.call(this, event.request, event.session, response);
+    },
+
+    IntentRequest: function (event, context, response) {
+        this.eventHandlers.onIntent.call(this, event.request, event.session, response);
+    },
+
+    SessionEndedRequest: function (event, context) {
+        this.eventHandlers.onSessionEnded(event.request, event.session);
+        context.succeed();
+    }
+};
 
 /**
+ * Override any of the eventHandlers as needed
+ */
+AlexaSkill.prototype.eventHandlers = {
+    /**
+     * Called when the session starts.
+     * Subclasses could have overriden this function to open any necessary resources.
+     */
+    onSessionStarted: function (sessionStartedRequest, session) {
+    },
+
+    /**
+     * Called when the user invokes the skill without specifying what they want.
+     * The subclass must override this function and provide feedback to the user.
+     */
+    onLaunch: function (launchRequest, session, response) {
+        throw "onLaunch should be overriden by subclass";
+    },
+
+    /**
+     * Called when the user specifies an intent.
+     */
+    onIntent: function (intentRequest, session, response) {
+        var intent = intentRequest.intent,
+            intentName = intentRequest.intent.name,
+            intentHandler = this.intentHandlers[intentName];
+        if (intentHandler) {
+            console.log('dispatch intent = ' + intentName);
+            intentHandler.call(this, intent, session, response);
+        } else {
+            throw 'Unsupported intent = ' + intentName;
+        }
+    },
+
+    /**
+     * Called when the user ends the session.
+     * Subclasses could have overriden this function to close any open resources.
+     */
+    onSessionEnded: function (sessionEndedRequest, session) {
+    }
+};
+
+/**
+ * Subclasses should override the intentHandlers with the functions to handle specific intents.
+ */
+AlexaSkill.prototype.intentHandlers = {};
+
+AlexaSkill.prototype.execute = function (event, context) {
+    try {
+        console.log("session applicationId: " + event.session.application.applicationId);
+
+        // Validate that this request originated from authorized source.
+        if (this._appId && event.session.application.applicationId !== this._appId) {
+            console.log("The applicationIds don't match : " + event.session.application.applicationId + " and "
+                + this._appId);
+            throw "Invalid applicationId";
+        }
+
+        if (!event.session.attributes) {
+            event.session.attributes = {};
+        }
+
+        if (event.session.new) {
+            this.eventHandlers.onSessionStarted(event.request, event.session);
+        }
+
+        // Route the request to the proper handler which may have been overriden.
+        var requestHandler = this.requestHandlers[event.request.type];
+        requestHandler.call(this, event, context, new Response(context, event.session));
+    } catch (e) {
+        console.log("Unexpected exception " + e);
+        context.fail(e);
+    }
+};
+
+var Response = function (context, session) {
+    this._context = context;
+    this._session = session;
+};
+
+function createSpeechObject(optionsParam) {
+    if (optionsParam && optionsParam.type === 'SSML') {
+        return {
+            type: optionsParam.type,
+            ssml: optionsParam.speech
+        };
+    } else {
+        return {
+            type: optionsParam.type || 'PlainText',
+            text: optionsParam.speech || optionsParam
+        }
+    }
+}
+
+Response.prototype = (function () {
+    var buildSpeechletResponse = function (options) {
+        var alexaResponse = {
+            outputSpeech: createSpeechObject(options.output),
+            shouldEndSession: options.shouldEndSession
+        };
+        if (options.reprompt) {
+            alexaResponse.reprompt = {
+                outputSpeech: createSpeechObject(options.reprompt)
+            };
+        }
+        if (options.cardTitle && options.cardContent) {
+            alexaResponse.card = {
+                type: "Simple",
+                title: options.cardTitle,
+                content: options.cardContent
+            };
+        }
+        var returnResult = {
+                version: '1.0',
+                response: alexaResponse
+        };
+        if (options.session && options.session.attributes) {
+            returnResult.sessionAttributes = options.session.attributes;
+        }
+        return returnResult;
+    };
+
+    return {
+        tell: function (speechOutput) {
+            this._context.succeed(buildSpeechletResponse({
+                session: this._session,
+                output: speechOutput,
+                shouldEndSession: true
+            }));
+        },
+        tellWithCard: function (speechOutput, cardTitle, cardContent) {
+            this._context.succeed(buildSpeechletResponse({
+                session: this._session,
+                output: speechOutput,
+                cardTitle: cardTitle,
+                cardContent: cardContent,
+                shouldEndSession: true
+            }));
+        },
+        ask: function (speechOutput, repromptSpeech) {
+            this._context.succeed(buildSpeechletResponse({
+                session: this._session,
+                output: speechOutput,
+                reprompt: repromptSpeech,
+                shouldEndSession: false
+            }));
+        },
+        askWithCard: function (speechOutput, repromptSpeech, cardTitle, cardContent) {
+            this._context.succeed(buildSpeechletResponse({
+                session: this._session,
+                output: speechOutput,
+                reprompt: repromptSpeech,
+                cardTitle: cardTitle,
+                cardContent: cardContent,
+                shouldEndSession: false
+            }));
+        }
+    };
+})();
+
+//-----------------------------------------------------------------------------------------------------
  * App ID for the skill
  */
 var APP_ID = "amzn1.echo-sdk-ams.app.e22256b3-2fe6-45e2-a84b-037e4edfdf4c";
-
-/**
- * Array containing BEER facts.
- */
-var BEER_FACTS = [
-    "You can't be a real country unless you have a beer and an airline. It helps if you have some kind of a football team, or some nuclear weapons, but at the very least you need a beer. Frank Zappa.",
-    "Let a man walk ten miles steadily on a hot summer's day along a dusty English road, and he will soon discover why beer was invented. Gilbert Chesterton.",
-    "I've only been in love with a beer bottle and a mirror. Sid Vicious.",
-    "They who drink beer will think beer. Washington Irving.",
-    "I'm gaining weight the right way: I'm drinking beer. Johnny Damon.",
-    "The basic thing a man should know is how to change a tire and how to drive a tractor. Whatever that bearded dude is doing on the Dos Equis beer commercials sets the bar. That's your guy. Every man should be aiming to be like him. The beard is just the tip of the iceberg. Timothy Olyphant.",
-    "Give a man a beer, waste an hour. Teach a man to brew, and waste a lifetime! Bill Owen.",
-    "Beer, it's the best damn drink in the world. Jack Nicholson.",
-    "He was a wise man who invented beer. Plato.",
-    "Beauty is in the eye of the beer holder. Kinky Friedman.",
-    "Milk is for babies. When you grow up you have to drink beer. Arnold Schwarzenegger.",
-    "Yes, sir. I'm a real Southern boy. I got a red neck, white socks, and Blue Ribbon beer. Billy Carter.",
-    "There's something sexy about a gut. Not a 400-pound beer gut, but a little paunch. I love that. Sandra Bullock.",
-    "Not all chemicals are bad. Without chemicals such as hydrogen and oxygen, for example, there would be no way to make water, a vital ingredient in beer. Dave Barry.",
-    "The head sticking to the side of your pint glass? That is referred to as lacing and is a sign of a clean glass and a good brew.",
-    "Your tongue has about 10,000 taste buds, so put them to good work and try out a new beer!",
-    "Not all waters are created equal: the origin and treatment of water have an impact on the finished beer, at both the molecular and sensory levels.",
-    "At the present time, all-malt beers are predominantly brewed with two-row barley, which yields plumper kernels and grows well in cooler climates.",
-    "Kilning is the source of nearly all malt flavors in beer.",
-    "Hops must be boiled vigorously to extract their bitterness.",
-    "Yeast is remarkably sensitive to temperature and can create completely different beers with only a small temperature variation in the brewing process.",
-    "To achieve the ultimate draft pint, pour the beer directly into the bottom of a perfectly clean glass. Continue to pour, letting the foam rise and settle until the glass is full, effectively creating a dense and long lasting head on the pint.",
-    "When pairing food and beer, be aware that a hoppy bitter flavor emphasizes spicy dishes."
-];
-/**
- * The AlexaSkill prototype and helper functions
- */
-var AlexaSkill = require('./AlexaSkill');
-
-/**
- * BeerQuotes is a child of AlexaSkill.
- * To read more about inheritance in JavaScript, see the link below.
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
- */
+var BEER_FACTS = [];
 var BeerQuotes = function () {
     AlexaSkill.call(this, APP_ID);
 };
@@ -114,6 +272,27 @@ BeerQuotes.prototype.intentHandlers = {
  * Gets a random new fact from the list and returns to the user.
  */
 function handleNewFactRequest(response) {
+    var s3 = new aws.S3();
+
+    var triviaObject = "beer_facts.json";
+    var getParams = {Bucket : bucketName, 
+    Key : triviaObject };
+
+    s3.getObject(getParams, function(err, data) {
+    if(err)
+        console.log('Error getting history data : ' + err);
+    else {
+// data retrieval worked... go through and dynamically load questions
+        var questionArray = eval("("+data.Body+")" );
+// parse through the object and load each question individually
+// file is stored in S3 Bucket in .json format 
+        var i;
+        for (i = 0; i < questionArray.length; i++) {
+//iterate through each fact in list and load them into bucket
+        BEER_FACTS.push(questionArray[i]);
+// pushes them to list
+}
+
     // Get a random BEER fact from the BEER facts list
     var factIndex = Math.floor(Math.random() * BEER_FACTS.length);
     var fact = BEER_FACTS[factIndex];
